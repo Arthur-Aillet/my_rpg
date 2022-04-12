93fe0f9743a12e10b697b2e1c37da80a8c6307a4
Aillet <arthur.aillet@epitech.eu>
2022-04-12 20:28:00 +0200

    [EDIT] potion loop debuged

--- src/mini_game/potion/potions_loop.c
+++ src/mini_game/potion/potions_loop.c
@@ -139,7 +139,7 @@ void hammer_loop(window_t *window, int *keys, object *mouse, int difficulty)
             sfClock_restart(clock);
         }
         hammer_controls(elements, &start, keys);
-        display_hammer(elements, window, nbr_steps, current_step);
+        display_hammer(elements, window, nbr_steps, steps_down);
         update_particles(window->window, start);
         update_mouse_cursor(window->window, mouse);
         sfRenderWindow_display(window->window);

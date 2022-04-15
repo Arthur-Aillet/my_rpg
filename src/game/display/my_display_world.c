eaa056197c0e4133c28a01ebe5f3730e4183f1e9
Tom BARITEAU-PETER <tom.bariteau-peter@epitech.eu>
2022-04-15 22:13:28 +0200

    [EDIT] sfView don't go over the map

--- src/game/display/my_display_world.c
+++ src/game/display/my_display_world.c
@@ -7,9 +7,21 @@
 
 #include "my_rpg.h"
 
-void setup_camera(game_t *game)
+void setup_camera(game_t *game, int i)
 {
     game->game->pos_cam = game->game->player->pos;
+    if (game->game->pos_cam.x - game->game->cam_rect.width / 2 < 0)
+        game->game->pos_cam.x = game->game->cam_rect.width / 2;
+    if (game->game->pos_cam.y - game->game->cam_rect.height / 2 < 0)
+        game->game->pos_cam.y = game->game->cam_rect.height / 2;
+    if (game->game->pos_cam.x + game->game->cam_rect.width / 2 >
+        game->game->maps[i]->width * 64)
+        game->game->pos_cam.x = game->game->maps[i]->width * 64 -
+            game->game->cam_rect.width / 2;
+    if (game->game->pos_cam.y + game->game->cam_rect.height / 2 >
+        game->game->maps[i]->height * 64)
+        game->game->pos_cam.y = game->game->maps[i]->height * 64 -
+            game->game->cam_rect.height / 2;
     sfView_setCenter(game->game->cam, game->game->pos_cam);
     sfRenderWindow_setView(game->window->window, game->game->cam);
 }
@@ -22,5 +34,5 @@ void display_world(game_t *game)
         i++;
     display_map(game, game->game->maps[i], i);
     display_player(game, i);
-    setup_camera(game);
+    setup_camera(game, i);
 }

a0219e43840f73ac7a5bafb792352a41c17ab8c9
Tom BARITEAU-PETER <tom.bariteau-peter@epitech.eu>
2022-04-15 22:01:40 +0200

    [ADD] sfView to have a camera that follow the player

--- src/game/display/my_display_world.c
+++ src/game/display/my_display_world.c
@@ -7,6 +7,13 @@
 
 #include "my_rpg.h"
 
+void setup_camera(game_t *game)
+{
+    game->game->pos_cam = game->game->player->pos;
+    sfView_setCenter(game->game->cam, game->game->pos_cam);
+    sfRenderWindow_setView(game->window->window, game->game->cam);
+}
+
 void display_world(game_t *game)
 {
     int i = 0;
@@ -15,4 +22,5 @@ void display_world(game_t *game)
         i++;
     display_map(game, game->game->maps[i], i);
     display_player(game, i);
+    setup_camera(game);
 }

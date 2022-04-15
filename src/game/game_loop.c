61a724cba0a6516ed48afdd4680749bf84e29ab7
Tom BARITEAU-PETER <tom.bariteau-peter@epitech.eu>
2022-04-15 02:23:51 +0200

    [EDIT] balance player speed + [ADD] shift to sprint (PS : player command are on ZQSD and arrows)

--- src/game/game_loop.c
+++ src/game/game_loop.c
@@ -8,17 +8,40 @@
 #include "my_game_struct.h"
 #include "my.h"
 
+int more_than_one_key(game_t *game)
+{
+    int i = 0;
+
+    if (game->keys[sfKeyUp] || game->keys[sfKeyZ])
+        i++;
+    if (game->keys[sfKeyDown] || game->keys[sfKeyS])
+        i++;
+    if (game->keys[sfKeyLeft] || game->keys[sfKeyQ])
+        i++;
+    if (game->keys[sfKeyRight] || game->keys[sfKeyD])
+        i++;
+    if (i != 1)
+        return 1;
+    return 0;
+}
+
 void my_player_move(game_t *game)
 {
+    int spd = game->game->player->move_spd;
+
+    if (game->keys[sfKeyLShift])
+        spd *= 1.5;
+    if (more_than_one_key(game))
+        spd /= 1.2;
     game->keys = get_keyboard_input(game->keys, game->window->window);
     if (game->keys[sfKeyUp] || game->keys[sfKeyZ])
-        game->game->player->pos.y -= game->game->player->move_spd;
+        game->game->player->pos.y -= spd;
     if (game->keys[sfKeyDown] || game->keys[sfKeyS])
-        game->game->player->pos.y += game->game->player->move_spd;
+        game->game->player->pos.y += spd;
     if (game->keys[sfKeyLeft] || game->keys[sfKeyQ])
-        game->game->player->pos.x -= game->game->player->move_spd;
+        game->game->player->pos.x -= spd;
     if (game->keys[sfKeyRight] || game->keys[sfKeyD])
-        game->game->player->pos.x += game->game->player->move_spd;
+        game->game->player->pos.x += spd;
 }
 
 void my_player_actions(game_t *game)

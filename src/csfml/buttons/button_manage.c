4a2fb568b8a13e35438dff2cfca131c57773c71f
Aillet <arthur.aillet@epitech.eu>
2022-04-20 15:25:48 +0200

    [EDIT] button lib big update, finaly working, template menu

--- src/csfml/buttons/button_manage.c
+++ src/csfml/buttons/button_manage.c
@@ -24,26 +24,42 @@ void button_center_text(button_t *bouton)
     sfText_setPosition(bouton->text.text, sfSprite_getPosition(bouton->sprite));
 }
 
+void sf_text_set_size(sfText *text, sfVector2f size)
+{
+    sfFloatRect rect = sfText_getGlobalBounds(text);
+    sfFloatRect rect_local = sfText_getLocalBounds(text);
+
+    sfText_setLetterSpacing(text, size.x / rect_local.width);
+    sfText_setScale(text, VCF{1, size.y / rect.height});
+}
+
 void click_but(button_t *but, sfVector2f mouse_pos, sfFloatRect rect, char *keys)
 {
-    if (MOUSE_HOVER == true && LCLICK == RELEASE) {
-        but->already_clicked = false;
-        if (but->click != NULL)
-            sfSound_play(but->click);
-        sfText_scale(but->text.text, INVERT_VCF(but->scale_click));
-        sfSprite_scale(but->sprite, INVERT_VCF(but->scale_click));
+    sfVector2f current_size = VCF{sfSprite_getGlobalBounds(but->sprite).width, sfSprite_getGlobalBounds(but->sprite).height};
+
+    if (MOUSE_HOVER == false) {
+        sf_sprite_set_pixel_size(but->sprite, VCF{current_size.x - (current_size.x - but->original_scale.x) / 10, current_size.y});
+        sf_text_set_size(but->text.text, VCF{current_size.x - (current_size.x - but->original_scale.x) / 10, current_size.y});
+        current_size = VCF{sfSprite_getGlobalBounds(but->sprite).width, sfSprite_getGlobalBounds(but->sprite).height};
+        sf_sprite_set_pixel_size(but->sprite, VCF{current_size.x, current_size.y + (but->original_scale.y - current_size.y) / 10});
+        sf_text_set_size(but->text.text, VCF{current_size.x, current_size.y + (but->original_scale.y - current_size.y) / 10});
         button_center_text(but);
     }
-    if (MOUSE_HOVER == false && but->already_clicked == true) {
+    if (MOUSE_HOVER == true && !LCLICK) {
         but->already_clicked = false;
-        sfText_scale(but->text.text, INVERT_VCF(but->scale_click));
-        sfSprite_scale(but->sprite, INVERT_VCF(but->scale_click));
+        sf_sprite_set_pixel_size(but->sprite, VCF{current_size.x - (current_size.x - (but->scale_hover.x + but->original_scale.x)) / 10, current_size.y});
+        sf_text_set_size(but->text.text, VCF{current_size.x - (current_size.x - (but->scale_hover.x + but->original_scale.x)) / 10, current_size.y});
+        current_size = VCF{sfSprite_getGlobalBounds(but->sprite).width, sfSprite_getGlobalBounds(but->sprite).height};
+        sf_sprite_set_pixel_size(but->sprite, VCF{current_size.x, current_size.y - (current_size.y - (but->scale_hover.y + but->original_scale.y)) / 10});
+        sf_text_set_size(but->text.text, VCF{current_size.x, current_size.y - (current_size.y - (but->scale_hover.y + but->original_scale.y)) / 10});
         button_center_text(but);
     }
-    if (MOUSE_HOVER == true && LCLICK == PRESS && but->already_clicked == false) {
-        but->already_clicked = true;
-        sfText_scale(but->text.text, but->scale_click);
-        sfSprite_scale(but->sprite, but->scale_click);
+    if (MOUSE_HOVER == true && LCLICK) {
+        sf_sprite_set_pixel_size(but->sprite, VCF{current_size.x + ((but->scale_click.x + but->original_scale.x) - current_size.x) / 10, current_size.y});
+        sf_text_set_size(but->text.text, VCF{current_size.x + ((but->scale_click.x + but->original_scale.x) - current_size.x) / 10, current_size.y});
+        current_size = VCF{sfSprite_getGlobalBounds(but->sprite).width, sfSprite_getGlobalBounds(but->sprite).height};
+        sf_sprite_set_pixel_size(but->sprite, VCF{current_size.x, current_size.y + ((but->scale_click.y + but->original_scale.y) - current_size.y) / 10});
+        sf_text_set_size(but->text.text, VCF{current_size.x, current_size.y + ((but->scale_click.y + but->original_scale.y) - current_size.y) / 10});
         button_center_text(but);
     }
 }
@@ -56,12 +72,24 @@ void button_update_texture_state(sfVector2f mouse_pos, button_t *but, char *keys
     if (MOUSE_HOVER == false)
         sfSprite_setTextureRect(but->sprite,
             (sfIntRect){0, 0, size.x / 3, size.y});
-    if (MOUSE_HOVER == true && LCLICK == RELEASE)
+    if (MOUSE_HOVER == true && !LCLICK)
         sfSprite_setTextureRect(but->sprite,
             (sfIntRect){size.x / 3, 0, size.x / 3, size.y});
-    if (MOUSE_HOVER == true && LCLICK == PRESS)
+    if (MOUSE_HOVER == true && LCLICK)
         sfSprite_setTextureRect(but->sprite,
             (sfIntRect){size.x / 3 * 2, 0, size.x / 3, size.y});
+    if (MOUSE_HOVER == false || !LCLICK)
+        but->already_clicked = false;
+    if (MOUSE_HOVER == false)
+        but->already_hoverd = false;
+    if (MOUSE_HOVER == true && but->already_hoverd == false && but->hover != NULL) {
+        sfSound_play(but->hover);
+        but->already_hoverd = true;
+    }
+    if (MOUSE_HOVER == true && LCLICK && but->click != NULL && but->already_clicked == false) {
+        sfSound_play(but->click);
+        but->already_clicked = true;
+    }
 }
 
 void update_button(sfRenderWindow *window, button_t *but, char *keys)
@@ -69,20 +97,6 @@ void update_button(sfRenderWindow *window, button_t *but, char *keys)
     sfVector2f mouse_pos = get_global_mouse_pos(window);
     sfFloatRect rect = sfSprite_getGlobalBounds(but->sprite);
 
-    if (MOUSE_HOVER == false && but->already_hoverd == true) {
-        sfText_scale(but->text.text, INVERT_VCF(but->scale_hover));
-        sfSprite_scale(but->sprite, INVERT_VCF(but->scale_hover));
-        button_center_text(but);
-        but->already_hoverd = false;
-    }
-    if (MOUSE_HOVER == true && but->already_hoverd == false) {
-        if (but->hover != NULL)
-            sfSound_play(but->hover);
-        sfText_scale(but->text.text, but->scale_hover);
-        sfSprite_scale(but->sprite, but->scale_hover);
-        button_center_text(but);
-        but->already_hoverd = true;
-    }
     click_but(but, mouse_pos, rect, keys);
     button_update_texture_state(mouse_pos, but, keys);
     display_button(window, but);

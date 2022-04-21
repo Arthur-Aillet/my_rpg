7b6d0668afaef5b5effe9043b291f2d47d5afac4
Aillet <arthur.aillet@epitech.eu>
2022-04-21 02:05:00 +0200

    [EDIT] updated depracted functions of the button lib

--- src/csfml/buttons/button_setup2.c
+++ src/csfml/buttons/button_setup2.c
@@ -39,10 +39,34 @@ void button_setup_correct_texture(button_t *bouton, sfIntRect size)
 {
     sfSprite_setTexture(bouton->sprite, bouton->texture, sfFalse);
     if (size.height * 3 < (int) sfTexture_getSize(bouton->texture).y
-    || size.width * 3 < (int) sfTexture_getSize(bouton->texture).x)
+        || size.width * 3 < (int) sfTexture_getSize(bouton->texture).x)
         sfSprite_setTextureRect(bouton->sprite, (sfIntRect){0, 0,
             (float) sfTexture_getSize(bouton->texture).y / 3,
             sfTexture_getSize(bouton->texture).x});
     else
         sfSprite_setTextureRect(bouton->sprite, size);
+}
+
+void button_setup_texture(button_t *btn, sfIntRect size, sfTexture *texture)
+{
+    sfImage *image;
+    sfFloatRect rect;
+
+    btn->texture = sfTexture_copy(texture);
+    if (btn->texture == NULL) {
+        image = create_blank_image();
+        btn->texture = sfTexture_createFromImage(image, NULL);
+        sfImage_destroy(image);
+    }
+    button_setup_correct_texture(btn, size);
+    rect = sfSprite_getGlobalBounds(btn->sprite);
+    sfSprite_setOrigin(btn->sprite, VCF{rect.width / 2 / sfSprite_getScale(
+    btn->sprite).x, rect.height / 2 / sfSprite_getScale(btn->sprite).y});
+    btn->scale_click = VCF{rect.width * btn->scale_click.x,
+        rect.height * btn->scale_click.y};
+    btn->original_scale = VCF{rect.width, rect.height};
+    btn->original_text_scale = VCF{rect.width *
+        btn->original_text_scale.x, rect.height * btn->original_text_scale.y};
+    btn->scale_hover = VCF{rect.width * btn->scale_hover.x,
+        rect.height * btn->scale_hover.y};
 }

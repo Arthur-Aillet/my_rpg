2f24dcaf77904a2692d9571f41b3342bfbe2dd3c
Auguste FRATER <auguste.frater@epitech.eu>
2022-04-27 15:51:46 +0200

    [fix] fixed the rclick issue

--- src/inventory/inventory_swaps.c
+++ src/inventory/inventory_swaps.c
@@ -27,9 +27,8 @@ item_t *split_item(int origin, int dest, item_t *items, int number)
 {
     items[dest].quantity = number;
     items[origin].quantity = items[origin].quantity - number;
-    destroy_object(items[dest].obj);
-    items[dest].obj = create_textured_object(items[origin].obj->texture,
-        VCF sfSprite_getPosition(items[origin].obj->sprite), VCF {4, 4});
+    items[dest].obj->texture = sfTexture_copy(items[origin].obj->texture);
+    items[dest].obj->sprite = sfSprite_copy(items[origin].obj->sprite);
     items[dest].type = items[origin].type;
     items[dest].armor_type = items[origin].armor_type;
     items[dest].stack_size = items[origin].stack_size;
@@ -41,9 +40,8 @@ item_t *level_items(int dest, int origin, int max, item_t *items)
     if (items[dest].type == 0 && items[origin].type == 0)
         return (items);
     if (items[dest].type != items[origin].type) {
-        destroy_object(items[dest].obj);
-        items[dest].obj = create_textured_object(items[origin].obj->texture,
-        VCF sfSprite_getPosition(items[origin].obj->sprite), VCF {4, 4});
+        items[dest].obj->texture = sfTexture_copy(items[origin].obj->texture);
+        items[dest].obj->sprite = sfSprite_copy(items[origin].obj->sprite);
         items[dest].type = items[origin].type;
     }
     items[dest].quantity = items[origin].quantity - max + items[dest].quantity;

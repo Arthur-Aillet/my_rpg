1e1e1da01f28a96c8f97b8303228ccbbf9026bc2
Axel DENIS <axel.denis@epitech.eu>
2022-05-02 11:44:08 +0200

    [EDIT] finished create_enemies_array function and its dependencies

--- src/enemies/read_enemies.c
+++ src/enemies/read_enemies.c
@@ -9,12 +9,18 @@
 #include "enemies.h"
 #include "my.h"
 
+/*
+utilise le json pour créer un ennemi. e_place doit déjà être alouée.
+renvoie 1 si une erreur apparait.
+*/
 int json_to_enemy(json_obj_t *data, enemy_t *e_place)
 {
     char *sprite_path = get_str_by_name(data, "sprite");
 
     e_place->name = my_strdup(data->name);
     e_place->type = get_int_by_name(data, "type");
+    if (e_place->type == -1)
+        return 1;
     e_place->speed = get_int_by_name(data, "speed");
     e_place->dps = get_int_by_name(data, "dps");
     e_place->last_update = 0;
@@ -41,4 +47,6 @@ enemy_t *create_enemies_array(void)
         if (json_to_enemy(get_obj_by_index(enemies_src, i), &(e_array[i])))
             return NULL;
     }
+    e_array[e_count].type = -1;
+    return e_array;
 }

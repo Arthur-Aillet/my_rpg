ba567895c1455e8eb3b4fc44b5e379fdccafb5d3
Axel DENIS <axel.denis@epitech.eu>
2022-05-02 14:45:45 +0200

    [ADD] Added enemies display function

--- include/enemies.h
+++ include/enemies.h
@@ -8,6 +8,10 @@
 #pragma once
 
 #include "csfml_libs.h"
+#include "enemies.h"
+#include "my.h"
+#include "my_csfml_utils.h"
+#include "my_game_struct.h"
 
 typedef struct enemy_s {
     char *name;
@@ -27,3 +31,4 @@ typedef struct enemy_node_s {
 void displace_enemies(enemy_node_t *all, game_t *game);
 enemy_node_t *spawn_ennemies(int nb, enemy_t *types, game_t *game);
 enemy_t *create_enemies_array(void);
+void display_enemies(game_t *game, enemy_node_t *enemies);
--- src/enemies/display_enemies.c
+++ src/enemies/display_enemies.c
@@ -7,5 +7,14 @@
 
 #include "enemies.h"
 #include "my.h"
+#include "my_csfml_utils.h"
+#include "my_game_struct.h"
 
-
+void display_enemies(game_t *game, enemy_node_t *all)
+{
+    while (all->enemy.type != -1) {
+        if (all->enemy.type >= 0)
+            sfRenderWindow_drawSprite(game->window, all->enemy.sprite, NULL);
+        all = all->next;
+    }
+}
